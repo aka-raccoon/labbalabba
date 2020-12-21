f1fa9a5d9547a9cbbcaa6ebe6fc126d45a11fd24
racoon racoon <racoonx2p@gmail.com>
2020-12-21 22:23:15 +0100

    refactor(data.query): query to strapi

--- gatsby-theme-novela-labbalabba/src/gatsby/data/data.query.js
+++ gatsby-theme-novela-labbalabba/src/gatsby/data/data.query.js
@@ -163,3 +163,137 @@ module.exports.contentful = {
     }
   }`,
 };
+
+module.exports.strapi = {
+  articles: `{
+    articles: allStrapiArticle(sort: {fields: [published_at, title], order: DESC}, limit: 1000) {
+      edges {
+        node {
+          id
+          secret
+          title
+          author {
+            name
+          }
+          date: published_at(formatString: "MMMM Do, YYYY")
+          dateForSEO: published_at
+          timeToRead: duration
+          childStrapiArticleBody {
+            childMdx {
+              body
+            }
+          }
+          excerpt
+          subscription
+          hero {
+            full: childImageSharp {
+              fluid(maxWidth: 944, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            regular: childImageSharp {
+              fluid(maxWidth: 653, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            narrow: childImageSharp {
+              fluid(maxWidth: 457, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            seo: childImageSharp {
+              fixed(width: 1200, quality: 100) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }`,
+  podcasts: `{
+    podcasts: allStrapiPodcast(sort: {fields: [published_at, title], order: DESC}, limit: 1000) {
+      edges {
+        node {
+          id
+          secret
+          title
+          author {
+            name
+          }
+          date: published_at(formatString: "MMMM Do, YYYY")
+          dateForSEO: published_at
+          timeToRead: duration
+          childStrapiPodcastBody {
+            childMdx {
+              body
+            }
+          }
+          excerpt
+          audio {
+            publicURL
+          }
+          subscription
+          hero {
+            full: childImageSharp {
+              fluid(maxWidth: 944, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            regular: childImageSharp {
+              fluid(maxWidth: 653, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            narrow: childImageSharp {
+              fluid(maxWidth: 457, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            seo: childImageSharp {
+              fixed(width: 1200, quality: 100) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }`,
+  authors: `{
+    authors: allStrapiAuthor {
+      edges {
+        node {
+          bio
+          id
+          name
+          featured
+          fields {
+            authorsPage
+            slug
+          }
+          social {
+            url
+          }
+          avatar {
+            small: childImageSharp {
+              fluid(maxWidth: 50, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            medium: childImageSharp {
+              fluid(maxWidth: 100, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+            large: childImageSharp {
+              fluid(maxWidth: 328, quality: 100) {
+                ${GatsbyFluid_withWebp}
+              }
+            }
+          }
+        }
+      }
+    }
+  }`,
+};
